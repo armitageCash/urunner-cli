c5533cb0d3c3905305612b24d9fdd151ac4c87df
Andrew Castro <aacastro661989@gmail.com>
2024-10-05 13:51:00 -0500

    added controllerand repository support

--- index.js
+++ index.js
@@ -39,80 +39,98 @@ function createFile(filePath, content) {
 }
 
 function generateUseCaseScaffold(useCaseName) {
-  const basePath = path.join(process.cwd(), "src", "cases", useCaseName);
+  const basePath = path.join(process.cwd(), "src");
+  const casePath = path.join(basePath, "cases", useCaseName);
+  const controllerPath = path.join(basePath, "controllers");
+  const repositoryPath = path.join(basePath, "repositories");
+  const sharedPath = path.join(basePath, "shared");
 
   // Crear directorios
-  createDirectory(basePath);
-  createDirectory(path.join(basePath, "impl"));
-  createDirectory(path.join(basePath, "manager"));
-  createDirectory(path.join(basePath, "types"));
-  createDirectory(path.join(basePath, "__tests__"));
+  createDirectory(casePath);
+  createDirectory(path.join(casePath, "impl"));
+  createDirectory(path.join(casePath, "manager"));
+  createDirectory(path.join(casePath, "types"));
+  createDirectory(path.join(casePath, "__tests__"));
+  createDirectory(controllerPath);
+  createDirectory(repositoryPath);
+  createDirectory(sharedPath);
 
   // Crear archivos
-  const indexContent = `
-import "dotenv/config";
-import { createApp, UseCaseResult, UsecaseType } from "urunner-lib";
-import { Logger } from "@/shared/logger";
-import { ${useCaseName}Data, Input, Output } from "./types";
-import { ${capitalize(useCaseName)}ServiceImpl } from "./impl";
+  const controllerContent = `
+import { ${useCaseName}Data, Input } from "@/cases/${useCaseName}/types";
+import ${capitalize(
+    useCaseName
+  )}Repository from "@/repositories/${useCaseName}";
 
-interface Dependencies {
-  logger: Logger;
-  ${useCaseName}Service: ${capitalize(useCaseName)}ServiceImpl;
+export default class ${capitalize(useCaseName)}Controller {
+  ${useCaseName}Repository: ${capitalize(useCaseName)}Repository;
+
+  constructor() {
+    this.${useCaseName}Repository = new ${capitalize(useCaseName)}Repository();
+  }
+
+  async execute(params: Input): Promise<${useCaseName}Data> {
+    const result = await this.${useCaseName}Repository.findData(params);
+
+    if (result) {
+      return Promise.resolve(result);
+    }
+
+    return Promise.reject("Data not found");
+  }
 }
-
-type ${capitalize(useCaseName)}UsecaseType = UsecaseType<
-  Input,
-  Dependencies,
-  UseCaseResult<${useCaseName}Data>
->;
-
-const adapter = (fn: ${capitalize(
-    useCaseName
-  )}UsecaseType) => async (params: Input, dependencies: Dependencies) => {
-  return await fn(params, dependencies);
-};
-
-export const ${useCaseName}: ${capitalize(useCaseName)}UsecaseType = async (
-  params: Input,
-  dependencies: Dependencies
-) => {
-  const { logger: log, ${useCaseName}Service } = dependencies;
-  try {
-    const response = await ${useCaseName}Service.execute(params);
-    return {
-      data: response,
-      status: "success",
-      message: "${capitalize(useCaseName)} ejecutado correctamente.",
-    };
-  } catch (e) {
-    return {
-      data: null,
-      message: "Error al ejecutar ${useCaseName}",
-      status: "error",
-    };
-  }
-};
-
-const usecase${capitalize(
-    useCaseName
-  )} = createApp(adapter(${useCaseName})).attach(
-  (dependencies: Dependencies) => {
-    dependencies.logger = new Logger();
-    dependencies.${useCaseName}Service = new ${capitalize(
-    useCaseName
-  )}ServiceImpl();
-  }
-);
-
-export default usecase${capitalize(useCaseName)};
 `;
 
-  const managerContent = `
-import { ${useCaseName}Data, Input } from "../types";
+  const repositoryContent = `
+import { Input, ${useCaseName}Data } from "@/cases/${useCaseName}/types";
+import Repository from "@/shared/repository";
 
-export default interface ${capitalize(useCaseName)}ServiceManager {
-  ${useCaseName}(params: Input): Promise<${useCaseName}Data | undefined>;
+export default class ${capitalize(useCaseName)}Repository extends Repository {
+  constructor() {
+    super();
+  }
+
+  async findData(input: Input): Promise<${useCaseName}Data | undefined> {
+    return this.database
+      .instance<${useCaseName}Data>("TableName") // Especifique el nombre de la tabla aquí
+      .where(input)
+      .first();
+  }
+}
+`;
+
+  const baseRepositoryContent = `
+import DatabaseService from "@/services/knex";
+
+export default abstract class Repository {
+  database: DatabaseService;
+
+  constructor() {
+    this.database = new DatabaseService();
+  }
+
+  async find<T>(query: string): Promise<T[]> {
+    return this.database.instance.raw(query);
+  }
+
+  async findOne<T>(id: number): Promise<T | undefined> {
+    return this.database.instance<T>('table_name').where({ id }).first();
+  }
+
+  async createOne<T>(data: Partial<T>): Promise<T> {
+    const [id] = await this.database.instance<T>('table_name').insert(data);
+    return this.findOne<T>(id);
+  }
+
+  async updateOne<T>(id: number, data: Partial<T>): Promise<T | undefined> {
+    await this.database.instance<T>('table_name').where({ id }).update(data);
+    return this.findOne<T>(id);
+  }
+
+  async deleteOne(id: number): Promise<boolean> {
+    const deleted = await this.database.instance('table_name').where({ id }).delete();
+    return deleted > 0;
+  }
 }
 `;
 
@@ -130,38 +148,26 @@ export interface Output {
 }
 `;
 
-  const implContent = `
-import { ${useCaseName}Data, Input } from "../types";
-
-export default interface ${capitalize(useCaseName)}ServiceManager {
-  ${useCaseName}(params: Input): Promise<${useCaseName}Data | undefined>;
-}
-`;
-
-  const testContent = `
-import usecase${capitalize(useCaseName)} from '../index';
-
-describe('${capitalize(useCaseName)} Use Case', () => {
-  it('should execute successfully', async () => {
-    // Write your test here
-  });
-
-  it('should handle errors', async () => {
-    // Write your error handling test here
-  });
-});
-`;
-
-  createFile(path.join(basePath, "index.ts"), indexContent);
-  createFile(path.join(basePath, "manager", "index.ts"), managerContent);
-  createFile(path.join(basePath, "types", "index.ts"), typesContent);
-  createFile(path.join(basePath, "impl", "index.ts"), implContent);
   createFile(
-    path.join(basePath, "__tests__", `${useCaseName}.test.ts`),
-    testContent
+    path.join(controllerPath, `${useCaseName}Controller.ts`),
+    controllerContent
   );
+  createFile(
+    path.join(repositoryPath, `${useCaseName}Repository.ts`),
+    repositoryContent
+  );
+  createFile(path.join(sharedPath, "repository.ts"), baseRepositoryContent);
+  createFile(path.join(casePath, "types", "index.ts"), typesContent);
 
   console.log(
-    `Scaffold for use case '${useCaseName}' has been created successfully in src/cases/${useCaseName}.`
+    `Scaffold for use case '${useCaseName}' has been created successfully.`
   );
+  console.log(
+    `Controller created in src/controllers/${useCaseName}Controller.ts`
+  );
+  console.log(
+    `Repository created in src/repositories/${useCaseName}Repository.ts`
+  );
+  console.log(`Base Repository created in src/shared/repository.ts`);
+  console.log(`Types created in src/cases/${useCaseName}/types/index.ts`);
 }
